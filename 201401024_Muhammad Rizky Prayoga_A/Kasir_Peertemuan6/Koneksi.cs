ae92d61c10472f9f62785e30e8723f16e4687b33
Muhammad Rizky Prayoga_024 <mrpharyga2019@gmail.com>
2022-05-11 20:46:46 +0700

    Program Kasir s/d Validasi

---
+++ 201401024_Muhammad Rizky Prayoga_A/Kasir_Peertemuan6/Koneksi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Kasir_Peertemuan6
+{
+    internal class Koneksi1
+    {
+        public SqlConnection GetConn()
+        {
+            SqlConnection Conn = new SqlConnection
+            {
+                ConnectionString = "Data Source=DESKTOP-I8ILPVI\\SQLEXPRESS;Initial Catalog=Kasir;Integrated Security=True"
+            };
+            return Conn;
+
+        }
+    }
+}
---
+++ 201401024_Muhammad Rizky Prayoga_A/Kasir_Peertemuan6/MainForm.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace Kasir_Peertemuan6
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.GroupBox groupBox2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(26, 543);
+			this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(1083, 263);
+			this.dataGridView1.TabIndex = 13;
+			this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1CellClick);
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(26, 503);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(286, 35);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "DB.Kasir. Table : TBL_BARANG";
+			// 
+			// label2
+			// 
+			this.label2.BackColor = System.Drawing.Color.Transparent;
+			this.label2.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(26, 158);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(150, 35);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Kode Barang";
+			this.label2.Click += new System.EventHandler(this.Label2Click);
+			// 
+			// label3
+			// 
+			this.label3.BackColor = System.Drawing.Color.Transparent;
+			this.label3.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(26, 209);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(150, 35);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Nama Barang";
+			// 
+			// label4
+			// 
+			this.label4.BackColor = System.Drawing.Color.Transparent;
+			this.label4.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(26, 264);
+			this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(150, 35);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "Harga Jual";
+			this.label4.Click += new System.EventHandler(this.Label4Click);
+			// 
+			// label5
+			// 
+			this.label5.BackColor = System.Drawing.Color.Transparent;
+			this.label5.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(26, 315);
+			this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(150, 35);
+			this.label5.TabIndex = 5;
+			this.label5.Text = "Harga Beli";
+			// 
+			// label6
+			// 
+			this.label6.BackColor = System.Drawing.Color.Transparent;
+			this.label6.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(26, 365);
+			this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(150, 35);
+			this.label6.TabIndex = 6;
+			this.label6.Text = "Jumlah";
+			// 
+			// label7
+			// 
+			this.label7.BackColor = System.Drawing.Color.Transparent;
+			this.label7.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(26, 416);
+			this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(150, 35);
+			this.label7.TabIndex = 7;
+			this.label7.Text = "Satuan";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(199, 158);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(289, 26);
+			this.textBox1.TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(199, 209);
+			this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(910, 26);
+			this.textBox2.TabIndex = 2;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(199, 264);
+			this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(289, 26);
+			this.textBox3.TabIndex = 3;
+			this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox3KeyPress);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(199, 316);
+			this.textBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(290, 26);
+			this.textBox4.TabIndex = 4;
+			this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox4KeyPress);
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(199, 365);
+			this.textBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(290, 26);
+			this.textBox5.TabIndex = 5;
+			this.textBox5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox5KeyPress);
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.SpringGreen;
+			this.button1.Location = new System.Drawing.Point(513, 378);
+			this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(143, 69);
+			this.button1.TabIndex = 7;
+			this.button1.Text = "Simpan";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.Cyan;
+			this.button2.Location = new System.Drawing.Point(664, 378);
+			this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(143, 69);
+			this.button2.TabIndex = 8;
+			this.button2.Text = "Update";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.Red;
+			this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.button3.Location = new System.Drawing.Point(815, 378);
+			this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(143, 69);
+			this.button3.TabIndex = 9;
+			this.button3.Text = "Hapus";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.BackColor = System.Drawing.Color.Honeydew;
+			this.groupBox1.Controls.Add(this.textBox7);
+			this.groupBox1.Location = new System.Drawing.Point(513, 276);
+			this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.groupBox1.Size = new System.Drawing.Size(596, 75);
+			this.groupBox1.TabIndex = 10;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Pencarian Kode Barang";
+			// 
+			// textBox7
+			// 
+			this.textBox7.BackColor = System.Drawing.Color.White;
+			this.textBox7.Location = new System.Drawing.Point(28, 29);
+			this.textBox7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(543, 26);
+			this.textBox7.TabIndex = 12;
+			this.textBox7.TextChanged += new System.EventHandler(this.TextBox7TextChanged);
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.Color.White;
+			this.button4.Location = new System.Drawing.Point(966, 378);
+			this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(143, 69);
+			this.button4.TabIndex = 11;
+			this.button4.Text = "Refresh";
+			this.button4.UseVisualStyleBackColor = false;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(199, 416);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(293, 28);
+			this.comboBox1.TabIndex = 6;
+			// 
+			// label8
+			// 
+			this.label8.BackColor = System.Drawing.Color.Aquamarine;
+			this.label8.Font = new System.Drawing.Font("Bahnschrift Condensed", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(0, 42);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(1139, 78);
+			this.label8.TabIndex = 14;
+			this.label8.Text = "APLIKASI KASIR";
+			this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.BackColor = System.Drawing.Color.LightCyan;
+			this.groupBox2.Location = new System.Drawing.Point(0, 137);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(1139, 342);
+			this.groupBox2.TabIndex = 15;
+			this.groupBox2.TabStop = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+			this.BackColor = System.Drawing.Color.Azure;
+			this.ClientSize = new System.Drawing.Size(1133, 834);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.groupBox2);
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.MaximumSize = new System.Drawing.Size(1155, 890);
+			this.MinimumSize = new System.Drawing.Size(1155, 890);
+			this.Name = "MainForm";
+			this.Padding = new System.Windows.Forms.Padding(100);
+			this.Text = "KASIR";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
